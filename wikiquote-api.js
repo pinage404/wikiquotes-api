1f07c354b5119e3434d346f53d925270f3504942
Siddhesh Mhadnak <siddhesh.mhadnak@outlook.com>
2016-11-21 20:28:34 +0530

    ES6 compliant

--- wikiquote-api.js
+++ wikiquote-api.js
@@ -1,8 +1,7 @@
-var WikiquoteApi = (function() {
+const WikiquoteApi = (function() {
 
-  var wqa = {};
-
-  var API_URL = "http://en.wikiquote.org/w/api.php";
+  const API_URL = 'https://en.wikiquote.org/w/api.php';
+  let wqa = {};
 
   /**
    * Query based on "titles" parameter and return page id.
@@ -13,35 +12,35 @@ var WikiquoteApi = (function() {
   wqa.queryTitles = function(titles, success, error) {
     $.ajax({
       url: API_URL,
-      dataType: "jsonp",
+      dataType: 'jsonp',
       data: {
-        format: "json",
-        action: "query",
-        redirects: "",
-        titles: titles
+        format: 'json',
+        action: 'query',
+        redirects: '',
+        titles: titles,
       },
 
       success: function(result, status) {
-        var pages = result.query.pages;
-        var pageId = -1;
-        for(var p in pages) {
-          var page = pages[p];
+        const pages = result.query.pages;
+        let pageId = -1;
+        for (let p in pages) {
+          let page = pages[p];
           // api can return invalid recrods, these are marked as "missing"
-          if(!("missing" in page)) {
+          if (!('missing' in page)) {
             pageId = page.pageid;
             break;
           }
         }
-        if(pageId > 0) {
+        if (pageId > 0) {
           success(pageId);
         } else {
-          error("No results");
+          error('No results');
         }
       },
 
-      error: function(xhr, result, status){
-        error("Error processing your query");
-      }
+      error: function(xhr, result, status) {
+        error('Error processing your query');
+      },
     });
   };
 
@@ -55,32 +54,32 @@ var WikiquoteApi = (function() {
   wqa.getSectionsForPage = function(pageId, success, error) {
     $.ajax({
       url: API_URL,
-      dataType: "jsonp",
+      dataType: 'jsonp',
       data: {
-        format: "json",
-        action: "parse",
-        prop: "sections",
-        pageid: pageId
+        format: 'json',
+        action: 'parse',
+        prop: 'sections',
+        pageid: pageId,
       },
 
-      success: function(result, status){
-        var sectionArray = [];
-        var sections = result.parse.sections;
-        for(var s in sections) {
-          var splitNum = sections[s].number.split('.');
-          if(splitNum.length > 1 && splitNum[0] === "1") {
+      success: function(result, status) {
+        let sectionArray = [];
+        const sections = result.parse.sections;
+        for (const s in sections) {
+          const splitNum = sections[s].number.split('.');
+          if (splitNum.length > 1 && splitNum[0] === "1") {
             sectionArray.push(sections[s].index);
           }
         }
         // Use section 1 if there are no "1.x" sections
-        if(sectionArray.length === 0) {
-          sectionArray.push("1");
+        if (sectionArray.length === 0) {
+          sectionArray.push('1');
         }
         success({ titles: result.parse.title, sections: sectionArray });
       },
-      error: function(xhr, result, status){
-        error("Error getting sections");
-      }
+      error: function(xhr, result, status) {
+        error('Error getting sections');
+      },
     });
   };
 
@@ -88,7 +87,7 @@ var WikiquoteApi = (function() {
    * Get all quotes for a given section.  Most sections will be of the format:
    * <h3> title </h3>
    * <ul>
-   *   <li> 
+   *   <li>
    *     Quote text
    *     <ul>
    *       <li> additional info on the quote </li>
@@ -107,41 +106,41 @@ var WikiquoteApi = (function() {
   wqa.getQuotesForSection = function(pageId, sectionIndex, success, error) {
     $.ajax({
       url: API_URL,
-      dataType: "jsonp",
+      dataType: 'jsonp',
       data: {
-        format: "json",
-        action: "parse",
-        noimages: "",
+        format: 'json',
+        action: 'parse',
+        noimages: '',
         pageid: pageId,
-        section: sectionIndex
+        section: sectionIndex,
       },
 
-      success: function(result, status){
-        var quotes = result.parse.text["*"];
-        var quoteArray = []
+      success: function(result, status) {
+        const quotes = result.parse.text['*'];
+        let quoteArray = []
 
         // Find top level <li> only
-        var $lis = $(quotes).find('li:not(li li)');
-        $lis.each(function() {
+        const lis = $(quotes).find('li:not(li li)');
+        lis.each(function() {
           // Remove all children that aren't <b>
           $(this).children().remove(':not(b)');
-          var $bolds = $(this).find('b');
+          const bolds = $(this).find('b');
 
           // If the section has bold text, use it.  Otherwise pull the plain text.
-          if($bolds.length > 0) {
-            quoteArray.push($bolds.html());
+          if (bolds.length > 0) {
+            quoteArray.push(bolds.html());
           } else {
             quoteArray.push($(this).html());
           }
         });
         success({ titles: result.parse.title, quotes: quoteArray });
       },
-      error: function(xhr, result, status){
-        error("Error getting quotes");
-      }
+      error: function(xhr, result, status) {
+        error('Error getting quotes');
+      },
     });
   };
-  
+
   /**
    * Get Wikipedia page for specific section
    * Usually section 0 includes personal Wikipedia page link
@@ -149,31 +148,29 @@ var WikiquoteApi = (function() {
   wqa.getWikiForSection = function(title, pageId, sec, success, error) {
     $.ajax({
       url: API_URL,
-      dataType: "jsonp",
+      dataType: 'jsonp',
       data: {
-        format: "json",
-        action: "parse",
-        noimages: "",
+        format: 'json',
+        action: 'parse',
+        noimages: '',
         pageid: pageId,
-        section: sec
+        section: sec,
       },
 
-      success: function(result, status){
-		
-        var wikilink;
-		console.log('what is iwlink:'+result.parse.iwlinks);
-		var iwl = result.parse.iwlinks;
-		for(var i=0; i<(iwl).length; i++){
-			var obj = iwl[i];
-			if((obj["*"]).indexOf(title) != -1){
-				 wikilink = obj.url;
-			}
-		}
+      success: function(result, status) {
+        let wikilink;
+        const iwl = result.parse.iwlinks;
+        for (let i = 0; i < iwl.length; i++) {
+          const obj = iwl[i];
+          if ((obj['*']).indexOf(title) !== -1) {
+            wikilink = obj.url;
+          }
+        }
         success(wikilink);
       },
-      error: function(xhr, result, status){
-        error("Error getting quotes");
-      }
+      error: function(xhr, result, status) {
+        error('Error getting quotes');
+      },
     });
   };
   /**
@@ -182,21 +179,21 @@ var WikiquoteApi = (function() {
   wqa.openSearch = function(titles, success, error) {
     $.ajax({
       url: API_URL,
-      dataType: "jsonp",
+      dataType: 'jsonp',
       data: {
-        format: "json",
-        action: "opensearch",
+        format: 'json',
+        action: 'opensearch',
         namespace: 0,
-        suggest: "",
-        search: titles
+        suggest: '',
+        search: titles,
       },
 
-      success: function(result, status){
+      success: function(result, status) {
         success(result[1]);
       },
-      error: function(xhr, result, status){
-        error("Error with opensearch for " + titles);
-      }
+      error: function(xhr, result, status) {
+        error('Error with opensearch for ' + titles);
+      },
     });
   };
 
@@ -209,23 +206,25 @@ var WikiquoteApi = (function() {
    */
   wqa.getRandomQuote = function(titles, success, error) {
 
-    var errorFunction = function(msg) {
+    function errorFunction(msg) {
       error(msg);
-    };
+    }
 
-    var chooseQuote = function(quotes) {
-      var randomNum = Math.floor(Math.random()*quotes.quotes.length);
+    function chooseQuote(quotes) {
+      const randomNum = Math.floor(Math.random() * quotes.quotes.length);
       success({ titles: quotes.titles, quote: quotes.quotes[randomNum] });
-    };
+    }
 
-    var getQuotes = function(pageId, sections) {
-      var randomNum = Math.floor(Math.random()*sections.sections.length);
+    function getQuotes(pageId, sections) {
+      const randomNum = Math.floor(Math.random() * sections.sections.length);
       wqa.getQuotesForSection(pageId, sections.sections[randomNum], chooseQuote, errorFunction);
-    };
+    }
 
-    var getSections = function(pageId) {
-      wqa.getSectionsForPage(pageId, function(sections) { getQuotes(pageId, sections); }, errorFunction);
-    };
+    function getSections(pageId) {
+      wqa.getSectionsForPage(pageId, function(sections) {
+        getQuotes(pageId, sections);
+      }, errorFunction);
+    }
 
     wqa.queryTitles(titles, getSections, errorFunction);
   };
@@ -234,9 +233,9 @@ var WikiquoteApi = (function() {
    * Capitalize the first letter of each word
    */
   wqa.capitalizeString = function(input) {
-    var inputArray = input.split(' ');
-    var output = [];
-    for(s in inputArray) {
+    const inputArray = input.split(' ');
+    let output = [];
+    for (const s in inputArray) {
       output.push(inputArray[s].charAt(0).toUpperCase() + inputArray[s].slice(1));
     }
     return output.join(' ');
